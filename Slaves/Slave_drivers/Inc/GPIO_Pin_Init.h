23b7d76d90befe2fa78ebd0b6d9c84d038334973
Peter Incze <peter.incze@gmail.com>
2017-08-07 13:51:58 +0200

    GPIO_Pin_Init updated

--- Slaves/Slave_drivers/Inc/GPIO_Pin_Init.h
+++ Slaves/Slave_drivers/Inc/GPIO_Pin_Init.h
@@ -25,60 +25,18 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32l4xx_hal.h"
+//#include "../Src/GPIO_Pin_Init.c"
 
 /* Exported types ------------------------------------------------------------*/
 
 /* Exported constants --------------------------------------------------------*/
 
-/* Exported typedef -----------------------------------------------------------*/
-typedef struct {
-	GPIO_TypeDef *port;
-	uint16_t pin;
-} gpio_pins_t;
 
-typedef struct {
-	GPIO_TypeDef *port;
-	uint16_t pin;
-	uint32_t ADC_channel;
-}gpio_analog_pins_t;
 
 /* Exported macro ------------------------------------------------------------*/
 
 /* Exported variables --------------------------------------------------------*/
-const gpio_pins_t stm32l476rg_digital_pins[] = {
-                             //PIN: D0      RX
-                             //PIN: D1      TX
-	{GPIOA, GPIO_PIN_10},    //PIN: D2
-	{GPIOB, GPIO_PIN_3},     //PIN: D3      PWM
-	{GPIOB, GPIO_PIN_5},     //PIN: D4
-	{GPIOB, GPIO_PIN_4},     //PIN: D5      PWM
-	{GPIOB, GPIO_PIN_10},    //PIN: D6      PWM
-	{GPIOA, GPIO_PIN_8},     //PIN: D7
-	{GPIOA, GPIO_PIN_9},     //PIN: D8
-	{GPIOC, GPIO_PIN_7},     //PIN: D9      PWM
-	{GPIOB, GPIO_PIN_6},     //PIN: D10     PWM
-	{GPIOA, GPIO_PIN_7},     //PIN: D11     PWM
-	{GPIOA, GPIO_PIN_6},     //PIN: D12
-	{GPIOA, GPIO_PIN_5},     //PIN: D13
-	{GPIOB, GPIO_PIN_9},     //PIN: D14
-	{GPIOB, GPIO_PIN_8},     //PIN: D15
-	{GPIOA, GPIO_PIN_0},     //PIN: A0 - As a Digital PIN
-	{GPIOA, GPIO_PIN_1},     //PIN: A1 - As a Digital PIN
-	{GPIOA, GPIO_PIN_4},     //PIN: A2 - As a Digital PIN
-	{GPIOB, GPIO_PIN_0},     //PIN: A3 - As a Digital PIN
-	{GPIOC, GPIO_PIN_1},     //PIN: A4 - As a Digital PIN
-	{GPIOC, GPIO_PIN_0},     //PIN: A5 - As a Digital PIN
 
-};
-
-const gpio_analog_pins_t stm32l476rg_analog_pins[] = {
-	{GPIOA, GPIO_PIN_0, ADC_CHANNEL_5},    //PIN: A0 - ADC12_IN5
-	{GPIOA, GPIO_PIN_1, ADC_CHANNEL_6},    //PIN: A1 - ADC12_IN6
-	{GPIOA, GPIO_PIN_4, ADC_CHANNEL_9},    //PIN: A2 - ADC12_IN9
-	{GPIOB, GPIO_PIN_0, ADC_CHANNEL_15},   //PIN: A3 - ADC12_IN15
-	{GPIOC, GPIO_PIN_1, ADC_CHANNEL_2},    //PIN: A4 - ADC123_IN2 // GPIOB GPIO_PIN_9 - I2C1_SDA
-	{GPIOC, GPIO_PIN_0, ADC_CHANNEL_1},    //PIN: A5 - ADC123_IN1 // GPIOB GPIO_PIN_8 - I2C1_SCL
-};
 
 
 /* Exported functions ------------------------------------------------------- */
