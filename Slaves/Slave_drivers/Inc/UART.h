f9f44faf65f5860d487db5cd7a35e93a58f8b5ac
Peter Incze <peter.incze@gmail.com>
2017-08-09 17:16:37 +0200

    Add UART.c, UART.h

---
+++ Slaves/Slave_drivers/Inc/UART.h
@@ -0,0 +1,15 @@
+#ifndef UART_H_INCLUDED
+#define UART_H_INCLUDED
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32l4xx_hal.h"
+#include "stm32l4xx_nucleo.h"
+
+/* Exported types ------------------------------------------------------------*/
+UART_HandleTypeDef UartHandle;
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+
+
+#endif // UART_H_INCLUDED
