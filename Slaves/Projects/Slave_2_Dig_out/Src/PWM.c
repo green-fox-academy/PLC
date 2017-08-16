9921b3bca623fb667ed8388f7fc21c0afd2c03e5
Peter Incze <peter.incze@gmail.com>
2017-08-16 16:51:25 +0200

    add (peter): new type in struct

--- Slaves/Projects/Slave_2_Dig_out/Src/PWM.c
+++ Slaves/Projects/Slave_2_Dig_out/Src/PWM.c
@@ -36,33 +36,34 @@ void pwm_set_duty(float duty)
 typedef struct {
 	GPIO_TypeDef *port;
 	uint16_t pin;
+	GPIO_InitTypeDef *Alternate;
 } gpio_pins_t_pwm;
 
 
 /* Private variables ---------------------------------------------------------*/
 const gpio_pins_t_pwm stm32l476rg_digital_pins_pwm[] = {
-	{GPIOA, GPIO_PIN_3},     //PIN: D0
-	{GPIOA, GPIO_PIN_2},     //PIN: D1
-	{GPIOA, GPIO_PIN_10},    //PIN: D2
-	{GPIOB, GPIO_PIN_3},     //PIN: D3
-	{GPIOB, GPIO_PIN_5},     //PIN: D4
-	{GPIOB, GPIO_PIN_4},     //PIN: D5
-	{GPIOB, GPIO_PIN_10},    //PIN: D6
-	{GPIOA, GPIO_PIN_8},     //PIN: D7
-	{GPIOA, GPIO_PIN_9},     //PIN: D8
-	{GPIOC, GPIO_PIN_7},     //PIN: D9		PWM 	TIM3_CH2
-	{GPIOB, GPIO_PIN_6},     //PIN: D10		PWM		TIM4_CH1
-	{GPIOA, GPIO_PIN_7},     //PIN: D11		PWM		TIM17_CH1
-	{GPIOA, GPIO_PIN_6},     //PIN: D12
-	{GPIOA, GPIO_PIN_5},     //PIN: D13
-	{GPIOB, GPIO_PIN_9},     //PIN: D14
-	{GPIOB, GPIO_PIN_8},     //PIN: D15
-	{GPIOA, GPIO_PIN_0},     //PIN: A0 - As a Digital PIN
-	{GPIOA, GPIO_PIN_1},     //PIN: A1 - As a Digital PIN
-	{GPIOA, GPIO_PIN_4},     //PIN: A2 - As a Digital PIN
-	{GPIOB, GPIO_PIN_0},     //PIN: A3 - As a Digital PIN
-	{GPIOC, GPIO_PIN_1},     //PIN: A4 - As a Digital PIN
-	{GPIOC, GPIO_PIN_0},     //PIN: A5 - As a Digital PIN
+	{GPIOA, GPIO_PIN_3, 0},     //PIN: D0
+	{GPIOA, GPIO_PIN_2, 0},     //PIN: D1
+	{GPIOA, GPIO_PIN_10, 0},    //PIN: D2
+	{GPIOB, GPIO_PIN_3, 0},     //PIN: D3
+	{GPIOB, GPIO_PIN_5, 0},     //PIN: D4
+	{GPIOB, GPIO_PIN_4, 0},     //PIN: D5
+	{GPIOB, GPIO_PIN_10, 0},    //PIN: D6
+	{GPIOA, GPIO_PIN_8, 0},     //PIN: D7
+	{GPIOA, GPIO_PIN_9, 0},     //PIN: D8
+	{GPIOC, GPIO_PIN_7, GPIO_AF2_TIM3},     //PIN: D9		PWM 	TIM3_CH2
+	{GPIOB, GPIO_PIN_6, GPIO_AF2_TIM4},     //PIN: D10		PWM		TIM4_CH1
+	{GPIOA, GPIO_PIN_7, GPIO_AF14_TIM17},     //PIN: D11		PWM		TIM17_CH1
+	{GPIOA, GPIO_PIN_6, 0},     //PIN: D12
+	{GPIOA, GPIO_PIN_5, 0},     //PIN: D13
+	{GPIOB, GPIO_PIN_9, 0},     //PIN: D14
+	{GPIOB, GPIO_PIN_8, 0},     //PIN: D15
+	{GPIOA, GPIO_PIN_0, 0},     //PIN: A0 - As a Digital PIN
+	{GPIOA, GPIO_PIN_1, 0},     //PIN: A1 - As a Digital PIN
+	{GPIOA, GPIO_PIN_4, 0},     //PIN: A2 - As a Digital PIN
+	{GPIOB, GPIO_PIN_0, 0},     //PIN: A3 - As a Digital PIN
+	{GPIOC, GPIO_PIN_1, 0},     //PIN: A4 - As a Digital PIN
+	{GPIOC, GPIO_PIN_0, 0},     //PIN: A5 - As a Digital PIN
 };
 
 
@@ -70,7 +71,7 @@ const gpio_pins_t_pwm stm32l476rg_digital_pins_pwm[] = {
 
 /* Private functions ---------------------------------------------------------*/
 
-void gpio_clk_enabl_pwm(GPIO_TypeDef *port)
+void gpio_clk_enable_pwm(GPIO_TypeDef *port)
 {
 	if (port == GPIOA)
 		__HAL_RCC_GPIOA_CLK_ENABLE();
@@ -91,19 +92,20 @@ void gpio_clk_enabl_pwm(GPIO_TypeDef *port)
 
 /* ########## Functions for Digital pins ########## */
 
-void gpio_init_digital_pin(uint8_t pin_index, uint32_t mode, uint32_t pull)
+void gpio_init_digital_pin_pwm(uint8_t pin_index, uint32_t mode, uint32_t pull)
 {
-	GPIO_InitTypeDef gpio_init_structure;
+	GPIO_InitTypeDef gpio_init_structure_pwm;
 
 	// Set the clock
-	gpio_clk_enable(stm32l476rg_digital_pins[pin_index].port);
+	gpio_clk_enable_pwm(stm32l476rg_digital_pins_pwm[pin_index].port);
 
 	// Set the init structure
-	gpio_init_structure.Pin = stm32l476rg_digital_pins[pin_index].pin;
-	gpio_init_structure.Mode = mode;
-	gpio_init_structure.Pull = pull;
-	gpio_init_structure.Speed = GPIO_SPEED_FAST;
+	gpio_init_structure_pwm.Pin = stm32l476rg_digital_pins_pwm[pin_index].pin;
+	gpio_init_structure_pwm.Mode = mode;
+	gpio_init_structure_pwm.Pull = pull;
+	gpio_init_structure_pwm.Speed = GPIO_SPEED_FREQ_HIGH;
+	//gpio_init_structure_pwm.Alternate =;
 
 	// Init the pin
-	HAL_GPIO_Init(stm32l476rg_digital_pins[pin_index].port, &gpio_init_structure);
+	HAL_GPIO_Init(stm32l476rg_digital_pins_pwm[pin_index].port, &gpio_init_structure_pwm);
 }
