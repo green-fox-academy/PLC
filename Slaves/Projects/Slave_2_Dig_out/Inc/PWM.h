fe0c8d38e7aa884f344187fe752af97b1e3ed168
Peter Incze <peter.incze@gmail.com>
2017-08-16 17:09:31 +0200

    add (peter): pwm init function

--- Slaves/Projects/Slave_2_Dig_out/Inc/PWM.h
+++ Slaves/Projects/Slave_2_Dig_out/Inc/PWM.h
@@ -6,6 +6,28 @@
 TIM_HandleTypeDef pwm_handle;
 TIM_OC_InitTypeDef pwm_oc_init;
 
+/* ########## Functions for Digital pins ########## */
+
+/* Function name: GPIO_Set_PIN
+ * Function purpose: Set PIN to output or input
+ * Function input - GPIO_Pin_t *GPIO_Pin: This is the pin to be set
+ * Function input - uint32_t pode: This is the mode it can be these:
+ * 		GPIO_MODE_INPUT:		Input Floating Mode
+ * 		GPIO_MODE_OUTPUT_PP: 	Output Push Pull Mode
+ * 		GPIO_MODE_OUTPUT_OD: 	Output Open Drain Mode
+ * 		GPIO_MODE_AF_PP:		Alternate Function Push Pull Mode
+ *		GPIO_MODE_AF_OD:		Alternate Function Open Drain Mode
+ * Function input - uint32_t pull can be:
+ * 		GPIO_PULLDOWN
+ * 		GPIO_PULLUP
+ * 		GPIO_NOPULL
+ * Function input - Alternate can be:
+ *
+ *
+ */
+
+void gpio_init_digital_pin_pwm(uint8_t pin_index, uint32_t mode, uint32_t pull);
+
 void pwm_init();
 void pwm_set_duty(float duty);
 
