51182684e2c67d4af31b2fe38fcfe85361d9d751
RasztovichGy <gyula.rasztovich@gmail.com>
2017-08-16 17:30:37 +0200

    feat(Gyula): UART_for_salve

--- Slaves/Projects/UART/Src/main.c
+++ Slaves/Projects/UART/Src/main.c
@@ -1,28 +1,38 @@
-/**
+/** UART in polling mode between L4-RealTerm terminal
   ******************************************************************************
   * @file    Templates/Src/main.c 
   * @author  Gyula
   * @version V1.8.0
   * @date    15-08-2017
-  * @brief   UART init - goal: show text on terminal
-  * 			mode: polling
+  * @brief   UART in polling mode between L4 - RealTerm terminal
+  * 		 Base: L4 Cube template file
   *******************************************************************************
   */
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
+#include <string.h>
 
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
 UART_HandleTypeDef uart_handle;
+// Buffer used for reception
+uint8_t buff_for_receive[100];
 
 /* Private function prototypes -----------------------------------------------*/
 static void SystemClock_Config(void);
 
 /* Private functions ---------------------------------------------------------*/
+#ifdef __GNUC__
+// With GCC/RAISONANCE, small printf (option LD Linker->Libraries->Small printf
+//  set to 'Yes') calls __io_putchar()
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif // __GNUC__
+
 
 /**
   * @brief  Main program
@@ -32,45 +42,92 @@ static void SystemClock_Config(void);
 
 int main(void)
 {
+	/* STM32L4xx HAL library initialization:
+	   - Configure the Flash prefetch, Flash preread and Buffer caches
+	   - Systick timer is configured by default as source of time base, but user
+			 can eventually implement his proper time base source (a general purpose
+			 timer for example or other time source), keeping in mind that Time base
+			 duration should be kept 1ms since PPP_TIMEOUT_VALUEs are defined and
+			 handled in milliseconds basis.
+	   - Low Level Initialization
+	 */
+	HAL_Init();
 
-  /* STM32L4xx HAL library initialization:
-       - Configure the Flash prefetch, Flash preread and Buffer caches
-       - Systick timer is configured by default as source of time base, but user 
-             can eventually implement his proper time base source (a general purpose 
-             timer for example or other time source), keeping in mind that Time base 
-             duration should be kept 1ms since PPP_TIMEOUT_VALUEs are defined and 
-             handled in milliseconds basis.
-       - Low Level Initialization
-     */
-  HAL_Init();
+	/* Configure the System clock to have a frequency of 80 MHz */
+	SystemClock_Config();
 
-  /* Configure the System clock to have a frequency of 80 MHz */
-  SystemClock_Config();
+	// Init LED and pushbutton
+	BSP_LED_Init(LED2);
+	BSP_PB_Init(BUTTON_USER, BUTTON_MODE_GPIO);
 
+	// Init D0/RX/PA3/USART2_RX and D1/TX/PA2/USART2_TX pins
+	GPIO_InitTypeDef GPIO_InitStruct;
+	__GPIOA_CLK_ENABLE();
+	GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3;
+	GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+	GPIO_InitStruct.Pull = GPIO_PULLUP;
+	GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
+	GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  // Init LED and pushbutton
-  BSP_LED_Init(LED2);
-  BSP_PB_Init(BUTTON_USER, BUTTON_MODE_GPIO);
+	// Set up UART
+	__USART2_CLK_ENABLE();
+	uart_handle.Instance        = USART2;				// UART -> D0 PA3 USART2_RX, D1 PA2 USART2_TX
+	uart_handle.Init.BaudRate   = 9600;				//
+	uart_handle.Init.WordLength = UART_WORDLENGTH_8B;	//
+	uart_handle.Init.StopBits   = UART_STOPBITS_1;	//
+	uart_handle.Init.Parity     = UART_PARITY_NONE;	//
+	uart_handle.Init.HwFlowCtl  = UART_HWCONTROL_NONE;//
+	uart_handle.Init.Mode       = UART_MODE_TX_RX;	//
+	uart_handle.Init.OverSampling = UART_OVERSAMPLING_16; //
+	HAL_UART_Init(&uart_handle);
 
-  // Set up UART
-  uart_handle.Instance        = USART2;				// UART -> D0 PA3 USART2_RX, D1 PA2 USART2_TX
-  uart_handle.Init.BaudRate   = 9600;
-  uart_handle.Init.WordLength = UART_WORDLENGTH_8B;
-  uart_handle.Init.StopBits   = UART_STOPBITS_1;
-  uart_handle.Init.Parity     = UART_PARITY_NONE;
-  uart_handle.Init.HwFlowCtl  = UART_HWCONTROL_NONE;
-  uart_handle.Init.Mode       = UART_MODE_TX_RX;
+	/* Loop */
+	uint8_t i = 0;
+	while (i < 10)
+	{
+/*	if (BSP_PB_GetState(BUTTON_USER == 1)) {
+		BSP_LED_On(LED2);
+		char msg[] = "UART test: char text send to terminal\r\n";
+		HAL_UART_Transmit(&uart_handle, msg, strlen(msg), 100);
+		//HAL_Delay(500);
+	} else {
+		//if (BSP_PB_GetState(BUTTON_USER == 0)) {
+		BSP_LED_Off(LED2);
+		HAL_UART_Transmit(&uart_handle, "No msg from board\r\n", 50, 100);
+	}
+*/
+	// Output without printf, using HAL function
+	char msg[] = "UART test - char text send to terminal with HAL_UART_Transmit.\r\n";
+	HAL_UART_Transmit(&uart_handle, msg, strlen(msg), 100);
+	BSP_LED_On(LED2);
 
-  // Init digital pins
-  // D0 PA3 USART2_RX, D1 PA2 USART2_TX
+	// Receive txt from Terminal
+	HAL_UART_Receive(&uart_handle, (uint8_t *)buff_for_receive, 100, 100);
 
+	// Output a message using printf function
+	printf("UART Printf Example: retarget the C library printf function to the UART\r\n");
+	printf("** Test finished successfully. ** \r\n");
+	HAL_Delay(500);
+	i++;
+	}
 
+	HAL_UART_Transmit(&uart_handle, "Test done\r\n", 12, 100);
+	BSP_LED_Off(LED2);
+}
 
-  /* Infinite loop */
-  while (1)
-  {
+/**
+  * @brief  Retargets the C library printf function to the USART.
+  * @param  None
+  * @retval None
+  */
 
-  }
+PUTCHAR_PROTOTYPE
+{
+  // Place your implementation of fputc here
+  // e.g. write a character to the EVAL_COM1 and Loop until the end of transmission
+  HAL_UART_Transmit(&uart_handle, (uint8_t *)&ch, 1, 0xFFFF);
+  return ch;
 }
 
 /**
