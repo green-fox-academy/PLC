23c06451ca4e647d5c75d0e174e83b84e1720482
Peter Incze <peter.incze@gmail.com>
2017-08-16 09:57:25 +0200

    feat (peter): PWM structure, const...

--- Slaves/Projects/Slave_2_Dig_out/Src/PWM.c
+++ Slaves/Projects/Slave_2_Dig_out/Src/PWM.c
@@ -30,3 +30,80 @@ void pwm_set_duty(float duty)
 	HAL_TIM_PWM_ConfigChannel(&pwm_handle, &pwm_oc_init, TIM_CHANNEL_2);
 	HAL_TIM_PWM_Start(&pwm_handle, TIM_CHANNEL_2);
 }
+
+
+
+typedef struct {
+	GPIO_TypeDef *port;
+	uint16_t pin;
+} gpio_pins_t_pwm;
+
+
+/* Private variables ---------------------------------------------------------*/
+const gpio_pins_t_pwm stm32l476rg_digital_pins_pwm[] = {
+	{GPIOA, GPIO_PIN_3},     //PIN: D0
+	{GPIOA, GPIO_PIN_2},     //PIN: D1
+	{GPIOA, GPIO_PIN_10},    //PIN: D2
+	{GPIOB, GPIO_PIN_3},     //PIN: D3
+	{GPIOB, GPIO_PIN_5},     //PIN: D4
+	{GPIOB, GPIO_PIN_4},     //PIN: D5
+	{GPIOB, GPIO_PIN_10},    //PIN: D6
+	{GPIOA, GPIO_PIN_8},     //PIN: D7
+	{GPIOA, GPIO_PIN_9},     //PIN: D8
+	{GPIOC, GPIO_PIN_7},     //PIN: D9		PWM 	TIM3_CH2
+	{GPIOB, GPIO_PIN_6},     //PIN: D10		PWM		TIM4_CH1
+	{GPIOA, GPIO_PIN_7},     //PIN: D11		PWM		TIM17_CH1
+	{GPIOA, GPIO_PIN_6},     //PIN: D12
+	{GPIOA, GPIO_PIN_5},     //PIN: D13
+	{GPIOB, GPIO_PIN_9},     //PIN: D14
+	{GPIOB, GPIO_PIN_8},     //PIN: D15
+	{GPIOA, GPIO_PIN_0},     //PIN: A0 - As a Digital PIN
+	{GPIOA, GPIO_PIN_1},     //PIN: A1 - As a Digital PIN
+	{GPIOA, GPIO_PIN_4},     //PIN: A2 - As a Digital PIN
+	{GPIOB, GPIO_PIN_0},     //PIN: A3 - As a Digital PIN
+	{GPIOC, GPIO_PIN_1},     //PIN: A4 - As a Digital PIN
+	{GPIOC, GPIO_PIN_0},     //PIN: A5 - As a Digital PIN
+};
+
+
+/* Private function prototypes -----------------------------------------------*/
+
+/* Private functions ---------------------------------------------------------*/
+
+void gpio_clk_enabl_pwm(GPIO_TypeDef *port)
+{
+	if (port == GPIOA)
+		__HAL_RCC_GPIOA_CLK_ENABLE();
+	else if (port == GPIOB)
+		__HAL_RCC_GPIOB_CLK_ENABLE();
+	else if (port == GPIOC)
+		__HAL_RCC_TIM3_CLK_ENABLE();
+	else if (port == GPIOD)
+		__HAL_RCC_GPIOD_CLK_ENABLE();
+	else if (port == GPIOE)
+		__HAL_RCC_GPIOE_CLK_ENABLE();
+	else if (port == GPIOF)
+		__HAL_RCC_GPIOF_CLK_ENABLE();
+	else if (port == GPIOG)
+		__HAL_RCC_GPIOG_CLK_ENABLE();
+}
+
+
+/* ########## Functions for Digital pins ########## */
+
+void gpio_init_digital_pin(uint8_t pin_index, uint32_t mode, uint32_t pull)
+{
+	GPIO_InitTypeDef gpio_init_structure;
+
+	// Set the clock
+	gpio_clk_enable(stm32l476rg_digital_pins[pin_index].port);
+
+	// Set the init structure
+	gpio_init_structure.Pin = stm32l476rg_digital_pins[pin_index].pin;
+	gpio_init_structure.Mode = mode;
+	gpio_init_structure.Pull = pull;
+	gpio_init_structure.Speed = GPIO_SPEED_FAST;
+
+	// Init the pin
+	HAL_GPIO_Init(stm32l476rg_digital_pins[pin_index].port, &gpio_init_structure);
+}
