24301d744373f14f3f0c95eacbd339ab07117f14
Peter Incze <peter.incze@gmail.com>
2017-08-21 15:26:25 +0200

    modify (peter): pwm clock function

--- Slaves/Projects/Slave_2_Dig_out/Src/PWM.c
+++ Slaves/Projects/Slave_2_Dig_out/Src/PWM.c
@@ -39,31 +39,35 @@ typedef struct {
 	GPIO_InitTypeDef *Alternate;
 } gpio_pins_t_pwm;
 
+typedef struct {
+
+};
+
 
 /* Private variables ---------------------------------------------------------*/
 const gpio_pins_t_pwm stm32l476rg_digital_pins_pwm[] = {
-	{GPIOA, GPIO_PIN_3, 0},     //PIN: D0
-	{GPIOA, GPIO_PIN_2, 0},     //PIN: D1
-	{GPIOA, GPIO_PIN_10, 0},    //PIN: D2
-	{GPIOB, GPIO_PIN_3, 0},     //PIN: D3
-	{GPIOB, GPIO_PIN_5, 0},     //PIN: D4
-	{GPIOB, GPIO_PIN_4, 0},     //PIN: D5
-	{GPIOB, GPIO_PIN_10, 0},    //PIN: D6
-	{GPIOA, GPIO_PIN_8, 0},     //PIN: D7
-	{GPIOA, GPIO_PIN_9, 0},     //PIN: D8
-	{GPIOC, GPIO_PIN_7, GPIO_AF2_TIM3},     //PIN: D9		PWM 	TIM3_CH2
-	{GPIOB, GPIO_PIN_6, GPIO_AF2_TIM4},     //PIN: D10		PWM		TIM4_CH1
-	{GPIOA, GPIO_PIN_7, GPIO_AF14_TIM17},     //PIN: D11		PWM		TIM17_CH1
-	{GPIOA, GPIO_PIN_6, 0},     //PIN: D12
-	{GPIOA, GPIO_PIN_5, 0},     //PIN: D13
-	{GPIOB, GPIO_PIN_9, 0},     //PIN: D14
-	{GPIOB, GPIO_PIN_8, 0},     //PIN: D15
-	{GPIOA, GPIO_PIN_0, 0},     //PIN: A0 - As a Digital PIN
-	{GPIOA, GPIO_PIN_1, 0},     //PIN: A1 - As a Digital PIN
-	{GPIOA, GPIO_PIN_4, 0},     //PIN: A2 - As a Digital PIN
-	{GPIOB, GPIO_PIN_0, 0},     //PIN: A3 - As a Digital PIN
-	{GPIOC, GPIO_PIN_1, 0},     //PIN: A4 - As a Digital PIN
-	{GPIOC, GPIO_PIN_0, 0},     //PIN: A5 - As a Digital PIN
+	{GPIOA, GPIO_PIN_3, 0},  				   	//PIN: D0
+	{GPIOA, GPIO_PIN_2, 0},						//PIN: D1
+	{GPIOA, GPIO_PIN_10, 0},					//PIN: D2
+	{GPIOB, GPIO_PIN_3, 0},   					//PIN: D3
+	{GPIOB, GPIO_PIN_5, 0},     				//PIN: D4
+	{GPIOB, GPIO_PIN_4, 0},     				//PIN: D5
+	{GPIOB, GPIO_PIN_10, 0},    				//PIN: D6
+	{GPIOA, GPIO_PIN_8, 0},     				//PIN: D7
+	{GPIOA, GPIO_PIN_9, 0},     				//PIN: D8
+	{GPIOC, GPIO_PIN_7, GPIO_AF2_TIM3},     	//PIN: D9		PWM 	TIM3_CH2
+	{GPIOB, GPIO_PIN_6, GPIO_AF2_TIM4},     	//PIN: D10		PWM		TIM4_CH1
+	{GPIOA, GPIO_PIN_7, GPIO_AF14_TIM17},     	//PIN: D11		PWM		TIM17_CH1
+	{GPIOA, GPIO_PIN_6, 0},     				//PIN: D12
+	{GPIOA, GPIO_PIN_5, 0},     				//PIN: D13
+	{GPIOB, GPIO_PIN_9, 0},     				//PIN: D14
+	{GPIOB, GPIO_PIN_8, 0},     				//PIN: D15
+	{GPIOA, GPIO_PIN_0, 0},     				//PIN: A0 - As a Digital PIN
+	{GPIOA, GPIO_PIN_1, 0},     				//PIN: A1 - As a Digital PIN
+	{GPIOA, GPIO_PIN_4, 0},     				//PIN: A2 - As a Digital PIN
+	{GPIOB, GPIO_PIN_0, 0},     				//PIN: A3 - As a Digital PIN
+	{GPIOC, GPIO_PIN_1, 0},     				//PIN: A4 - As a Digital PIN
+	{GPIOC, GPIO_PIN_0, 0},     				//PIN: A5 - As a Digital PIN
 };
 
 
@@ -87,6 +91,24 @@ void gpio_clk_enable_pwm(GPIO_TypeDef *port)
 		__HAL_RCC_GPIOF_CLK_ENABLE();
 	else if (port == GPIOG)
 		__HAL_RCC_GPIOG_CLK_ENABLE();
+}
+
+void pwm_clk_enable(GPIO_TypeDef *port, uint16_t pin)
+{
+	if (port == GPIOA && pin == GPIO_PIN_7)
+		__HAL_RCC_TIM17_CLK_ENABLE();
+	else if (port == GPIOB && pin == GPIO_PIN_6)
+		__HAL_RCC_TIM4_CLK_ENABLE();
+	else if (port == GPIOC && pin == GPIO_PIN_7)
+		__HAL_RCC_TIM3_CLK_ENABLE();
+	/*else if (port == GPIOD)
+		__HAL_RCC_GPIOD_CLK_ENABLE();
+	else if (port == GPIOE)
+		__HAL_RCC_GPIOE_CLK_ENABLE();
+	else if (port == GPIOF)
+		__HAL_RCC_GPIOF_CLK_ENABLE();
+	else if (port == GPIOG)
+		__HAL_RCC_GPIOG_CLK_ENABLE();*/
 }
 
 
