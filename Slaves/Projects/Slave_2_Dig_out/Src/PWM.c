9b8f1ec5320451a781e57977e6cf291ec1a25d06
Peter Incze <peter.incze@gmail.com>
2017-08-21 17:14:20 +0200

    modify (peter): pwm init

--- Slaves/Projects/Slave_2_Dig_out/Src/PWM.c
+++ Slaves/Projects/Slave_2_Dig_out/Src/PWM.c
@@ -31,14 +31,28 @@ void pwm_set_duty(float duty)
 	HAL_TIM_PWM_Start(&pwm_handle, TIM_CHANNEL_2);
 }
 
-void pwm_inti(uint8_t pin_index)
+void pwm_init(uint8_t pin_index)
 {
 	TIM_HandleTypeDef pwm_handle;
 	TIM_OC_InitTypeDef pwm_oc_init;
 
 	pwm_clk_enable(stm32l476rg_digital_pins_pwm[pin_index].Alternate);
 
+	pwm_handle.Instance = stm32l476rg_pwm_set[pin_index].tim;
+	pwm_handle.State = HAL_TIM_STATE_RESET;
+	pwm_handle.Channel = stm32l476rg_pwm_set[pin_index].channel;
+	pwm_handle.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+	pwm_handle.Init.CounterMode = TIM_COUNTERMODE_UP;
+	pwm_handle.Init.Period = 0xFFFF;
+	pwm_handle.Init.Prescaler = 0;
+	HAL_TIM_PWM_Init(&pwm_handle);
 
+	pwm_oc_init.OCFastMode = TIM_OCFAST_DISABLE;
+	pwm_oc_init.OCIdleState = TIM_OCIDLESTATE_RESET;
+	pwm_oc_init.OCMode = TIM_OCMODE_PWM1;
+	pwm_oc_init.OCPolarity = TIM_OCPOLARITY_HIGH;
+	pwm_oc_init.Pulse = 0xFFFF;
+	HAL_TIM_PWM_ConfigChannel(&pwm_handle, &pwm_oc_init, stm32l476rg_pwm_set[pin_index].tim_ch);
 }
 
 
