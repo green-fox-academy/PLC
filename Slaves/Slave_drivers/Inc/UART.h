94576d7a4833340a4c74af44edb1218e7e2807fb
Peter Incze <peter.incze@gmail.com>
2017-08-23 11:49:49 +0200

    add (peter): new uart.c and uart.h

--- Slaves/Slave_drivers/Inc/UART.h
+++ Slaves/Slave_drivers/Inc/UART.h
@@ -1,15 +1,43 @@
-#ifndef UART_H_INCLUDED
-#define UART_H_INCLUDED
+#ifndef __UART_H_
+#define __UART_H_
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32l4xx_hal.h"
 #include "stm32l4xx_nucleo.h"
+#include <string.h>
+#include <stdlib.h>
 
-/* Exported types ------------------------------------------------------------*/
-UART_HandleTypeDef UartHandle;
-/* Exported constants --------------------------------------------------------*/
-/* Exported macro ------------------------------------------------------------*/
-/* Exported functions ------------------------------------------------------- */
+#define TXBUFFERSIZE        32			// transmit
+#define RXBUFFERSIZE        32			// receive
+
+#define USARTx                           USART3
+#define USARTx_CLK_ENABLE()              __HAL_RCC_USART3_CLK_ENABLE()
+#define USARTx_RX_GPIO_CLK_ENABLE()      __HAL_RCC_GPIOC_CLK_ENABLE()
+#define USARTx_TX_GPIO_CLK_ENABLE()    	 __HAL_RCC_GPIOC_CLK_ENABLE()
+
+#define USARTx_FORCE_RESET()             __HAL_RCC_USART3_FORCE_RESET()
+#define USARTx_RELEASE_RESET()           __HAL_RCC_USART3_RELEASE_RESET()
+
+#define BAUDRATE						1000000
+
+/* Definition for USARTx Pins */
+#define USARTx_TX_PIN                    GPIO_PIN_10
+#define USARTx_TX_GPIO_PORT              GPIOC
+#define USARTx_TX_AF                     GPIO_AF7_USART3
+#define USARTx_RX_PIN                    GPIO_PIN_11
+#define USARTx_RX_GPIO_PORT              GPIOC
+#define USARTx_RX_AF                     GPIO_AF7_USART3
+
+UART_HandleTypeDef uart_handle;
+
+uint8_t TX_buffer[TXBUFFERSIZE];
+uint8_t RX_buffer[RXBUFFERSIZE];
+volatile uint8_t interrupt_flag;
+
+uint8_t slave_address;
+
+void modbus_init();
+void UART_send(uint8_t *buffer);
 
 
-#endif // UART_H_INCLUDED
+#endif /* __UART_H_ */
--- Slaves/Slave_drivers/Src/UART.c
+++ Slaves/Slave_drivers/Src/UART.c
@@ -1,51 +1,123 @@
-#include "UART.h"
+/* Includes ------------------------------------------------------------------*/
+#include "uart.h"
 
-uint8_t TxMessage[] = "UART Ready\n\r";
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
 
-#ifdef __GNUC__
-/* With GCC/RAISONANCE, small printf (option LD Linker->Libraries->Small printf
-   set to 'Yes') calls __io_putchar() */
-#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
-#else
-#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
-#endif /* __GNUC__ */
+void rx_tx_GPIO_init();
+void uart_init();
+void buffer_init();;
 
-void MX_GPIO_init()
+/* Private functions ---------------------------------------------------------*/
+void modbus_error_handler(uint8_t error);
+
+void UART_send(uint8_t *buffer)
 {
-	__GPIOA_CLK_ENABLE();
-	__USART2_CLK_ENABLE();
-}
+	// Send buffer content
+	HAL_UART_Transmit(&uart_handle, (uint8_t*) buffer, 32, 4);
 
-void pin_init()
-{
-    GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_LOW;
-    GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-}
-
-void UART2_init()
-{
-	UartHandle.Instance        = USART2;
-	UartHandle.Init.BaudRate   = 9600;
-	UartHandle.Init.WordLength = UART_WORDLENGTH_8B;
-	UartHandle.Init.StopBits   = UART_STOPBITS_1;
-	UartHandle.Init.Parity     = UART_PARITY_NONE;
-	UartHandle.Init.HwFlowCtl  = UART_HWCONTROL_NONE;
-	UartHandle.Init.Mode       = UART_MODE_TX_RX;
-	UartHandle.Init.OverSampling = UART_OVERSAMPLING_16;
-	UartHandle.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
-	UartHandle.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-	HAL_UART_Init(&UartHandle);
 }
 
 
-PUTCHAR_PROTOTYPE	// Retargets the C library printf function to the USART.
+void modbus_init()
 {
-  HAL_UART_Transmit(&uart_handle, (uint8_t *)&ch, 1, 0xFFFF);
-  return ch;
+	buffer_init();
+	rx_tx_GPIO_init();
+	uart_init();
 }
 
-HAL_UART_Transmit(&UartHandle, TxMessage, 255, 10);
+void buffer_init()
+{
+	for (int i = 0; i < RXBUFFERSIZE; i++) {
+		RX_buffer[i] = 0;
+	}
+}
+
+void uart_init()
+{
+	uart_handle.Instance 	   	= USARTx;
+	uart_handle.Init.BaudRate   = BAUDRATE;
+	uart_handle.Init.WordLength	= UART_WORDLENGTH_8B;
+	uart_handle.Init.StopBits  	= UART_STOPBITS_1;
+	uart_handle.Init.Parity     	= UART_PARITY_NONE;
+	uart_handle.Init.HwFlowCtl  	= UART_HWCONTROL_NONE;
+	uart_handle.Init.Mode       	= UART_MODE_TX_RX;
+	uart_handle.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+
+	if(HAL_UART_DeInit(&uart_handle) != HAL_OK)
+		;
+	if(HAL_UART_Init(&uart_handle) != HAL_OK)
+		;
+		// Error_
+
+
+	//Setup interrupts for UART
+	HAL_NVIC_SetPriority(USART3_IRQn, 0, 0);
+	HAL_NVIC_EnableIRQ(USART3_IRQn);
+
+	// Start UART receiver in interrupt mode
+	HAL_UART_Receive_IT(&uart_handle, RX_buffer, 32);
+
+	interrupt_flag = 0;
+}
+
+void rx_tx_GPIO_init()
+{
+	GPIO_InitTypeDef  GPIO_InitStruct;
+
+	/*##-1- Enable peripherals and GPIO Clocks ##*/
+	/* Enable GPIO TX/RX clock */
+	USARTx_TX_GPIO_CLK_ENABLE();
+	USARTx_RX_GPIO_CLK_ENABLE();
+
+
+	/* Enable USARTx clock */
+	USARTx_CLK_ENABLE();
+
+	/*##-2- Configure peripheral GPIO ##*/
+	/* UART TX GPIO pin configuration  */
+	GPIO_InitStruct.Pin       = USARTx_TX_PIN;
+	GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
+	GPIO_InitStruct.Pull      = GPIO_PULLUP;
+	GPIO_InitStruct.Speed     = GPIO_SPEED_HIGH;
+	GPIO_InitStruct.Alternate = USARTx_TX_AF;
+
+	HAL_GPIO_Init(USARTx_TX_GPIO_PORT, &GPIO_InitStruct);
+
+	/* UART RX GPIO pin configuration  */
+	GPIO_InitStruct.Pin = USARTx_RX_PIN;
+	GPIO_InitStruct.Alternate = USARTx_RX_AF;
+
+	HAL_GPIO_Init(USARTx_RX_GPIO_PORT, &GPIO_InitStruct);
+
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+	// This interrupt is fired when we received 20byte data via USART6
+
+	interrupt_flag = 1;
+	// Re-enable the interrupt
+	HAL_UART_Receive_IT(huart, RX_buffer, 32);
+}
+
+void modbus_error_handler(uint8_t error)
+{
+	switch (error) {
+
+	case 1 :
+		;
+		break;
+	case 2 :
+		;
+		break;
+	case 3 :
+		;
+		break;
+	default :
+		;
+	}
+}
