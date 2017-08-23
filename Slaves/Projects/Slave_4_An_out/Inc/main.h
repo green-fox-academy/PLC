02c173e4835293728fb2156861279ecaf23df56f
RasztovichGy <gyula.rasztovich@gmail.com>
2017-08-23 16:24:25 +0200

    feat(Gyula): DAC_in_progress

---
+++ Slaves/Projects/Slave_4_An_out/Inc/main.h
@@ -0,0 +1,22 @@
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32l4xx_hal.h"
+#include "stm32l4xx_nucleo.h"
+#include "stm32l476xx.h"
+#include "GPIO.h"
+#include "dac.h"
+#include "uart.h"
+#include "stdio.h"
+
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
